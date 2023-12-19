f21de55df9f7cd8df85d2cf85145c24ecbc2f25b
TakumaOnishi <e57a3b7fd778c@gmail.com>
2023-12-19 19:51:41 +0900

    雛形を作った

---
+++ remap/script.js
@@ -0,0 +1,20 @@
+// JSの簡単な例（テキストをクリックすると文字色が白↔︎黒にトグルする）
+window.addEventListener('DOMContentLoaded', () => { // 読み込みを待って処理を始める
+
+   const _example = document.getElementById("example"); // HTML要素を取得する
+
+   let textIsWhite = true; // 今のテキストの色をもつ変数
+
+   _example.addEventListener('click', function(){
+      textIsWhite = !textIsWhite; // trueならfalseに、falseならtrueにする
+      updateTextColor(); // 使い回したい処理は関数にまとめる
+   });
+
+   function updateTextColor(){ // CSSの内容を書き換える
+      if(textIsWhite){
+         _example.style.color = "#ffffff"
+      }else{
+         _example.style.color = "#000000"
+      }
+   }
+});
