c1bb6aae47b88f2eeccd9a89d6b8c70c709a4711
TakumaOnishi <e57a3b7fd778c@gmail.com>
2024-05-28 03:57:17 +0900

    policy

--- script.js
+++ script.js
@@ -3,13 +3,13 @@ window.addEventListener('DOMContentLoaded', () => {
    const _otaku = document.getElementsByClassName("otaku");
    const _ham = document.getElementById("ham");
 
-   for(let i=0; i<_otaku.length; i++){
-      _otaku[i].addEventListener('click', function(){
+   for (let i = 0; i < _otaku.length; i++) {
+      _otaku[i].addEventListener('click', function () {
          _otaku[i].classList.toggle("show");
       });
    }
 
-   _ham.addEventListener('click', function(){
+   _ham.addEventListener('click', function () {
       _ham.classList.toggle("show");
    });
 });
