fd9a974e670428b8a87cd1284c62df7b76199da4
TakumaOnishi <e57a3b7fd778c@gmail.com>
2024-06-10 05:14:52 +0900

    gallery

--- script.js
+++ script.js
@@ -2,6 +2,8 @@ window.addEventListener('DOMContentLoaded', () => {
 
    const _otaku = document.getElementsByClassName("otaku");
    const _ham = document.getElementById("ham");
+   const _gallery = document.getElementById("gallery");
+   const _g_img = document.getElementsByClassName("g_img");
 
    for (let i = 0; i < _otaku.length; i++) {
       _otaku[i].addEventListener('click', function () {
@@ -12,4 +14,11 @@ window.addEventListener('DOMContentLoaded', () => {
    _ham.addEventListener('click', function () {
       _ham.classList.toggle("show");
    });
+
+   for (let i = 0; i < _g_img.length; i++) {
+      _g_img[i].addEventListener('click', function () {
+         _gallery.prepend(_gallery.removeChild(this));
+      });
+   }
+
 });
