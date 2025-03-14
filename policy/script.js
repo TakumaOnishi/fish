44dbdb8fffe73abdcebbf11895a4c824eb815e24
大西拓磨 <e57a3b7fd778c@gmail.com>
2025-03-14 15:57:38 +0900

    英数・かなコンボをホームポジションに戻した

--- policy/script.js
+++
@@ -1,8 +0,0 @@
-window.addEventListener('DOMContentLoaded', () => {
-
-   const _ham = document.getElementById("ham");
-
-   _ham.addEventListener('click', function(){
-      _ham.classList.toggle("show");
-   });
-});
